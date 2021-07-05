b1e2be647c27da7506ec0d847adbd228c174ae56
siddanand <chandiboy18@gmail.com>
2021-07-05 06:47:54 +0530

    Displayed Github User Details
    User enters their github username and get all the information in json format

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
-import { useHistory } from "react-router";
 import { useRouter } from 'next/router';
 
 export default function Home() {
--- pages/list.js
+++ pages/list.js
@@ -1,8 +1,13 @@
 import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import User_info from "./user_info";
 
 export default function List() {
 const [list, setList] = useState([]);
 const [user, setUser] = useState('');
+const [userupdated, setUserUpdated] = useState('');
+const [status, setStatus] = useState(0);
+const router = useRouter();
 
   useEffect(() => {
 fetch('https://api.github.com/users')
@@ -13,15 +18,41 @@ fetch('https://api.github.com/users')
 })
     }, [])
 
+function onUsername(event){
+  let username = event.target.value;
+  console.log(username);
+  setUser(username);
+}
+function onSubmitHandler(e){
+e.preventDefault();
+setStatus(1);
+setUserUpdated(user);
+}
 return(
   <div>
   <div class="container">
+<div class="row">
+<div class="col-sm-6">
 <h1>List of User Types:</h1>
 {list.map((item)=>
   <div key={item.id}>
   {item.login}
   </div>
 )}
+</div>
+<div class="col-sm-6">
+<h1>Enter your Github user name:</h1>
+<form onSubmit={onSubmitHandler}>
+<input type="text" placeholder="Username" class="form-control" onChange={onUsername} required/>
+<br/>
+<button type="submit" class="btn btn-primary">Submit</button>
+</form>
+<br/>
+{status==1?(<div><User_info key={userupdated} user={userupdated}/></div>):(<div></div>)}
+</div>
+
+</div>
+
 </div>
 </div>
 
